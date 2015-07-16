2bfe83470f31379de11e91b33885a0022b4d50f4
Max Shishkin <angryobject@users.noreply.github.com>
2015-07-16 12:13:43 +0300

    Add babel polyfill

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,9 @@
+// load styles
 import 'normalize.css';
 import 'styles/main.sass';
+
+// import polyfills if needed
+import 'babel/polyfill';
 
 import React from 'react';
 import App from 'components/app';
