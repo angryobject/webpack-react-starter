f24d18f21e65a89c42c910a19e4ebb6bdaebe219
Max Shishkin <angryobject@users.noreply.github.com>
2015-10-13 12:25:05 +0300

    Use React.createClass instead of subclassing React.Component

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,19 +1,19 @@
-export default class App extends React.Component {
-   constructor() {
-      super();
+export default React.createClass({
 
-      this.state = {
+   getInitialState() {
+      return {
          name: 'App'
       };
-   }
+   },
 
    componentDidMount() {
       document.documentElement.classList.add('appIsMounted');
-   }
+   },
 
    render() {
       return <div className="App">
          <span className="AppName">{this.state.name}</span> component has rendered
       </div>;
    }
-}
+
+});
